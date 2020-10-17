f3f53cc25647e80d3aca90ea313d2a0a41e40785
tuhalang <tuhalang007@outlook.com.vn>
2020-10-17 17:09:08 +0700

    add file

---
+++ src/main/java/com/lazyyy/blog/controller/HomeController.java
@@ -0,0 +1,8 @@
+package com.lazyyy.blog.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("")
+public class HomeController extends HttpServlet {
+}
