16c3c70b7c61185923c0a979419823b019a379e5
tranvandiep010 <trandiep10121999@gmail.com>
2020-10-20 22:34:59 +0700

    .

--- src/main/java/com/lazyyy/blog/controller/HomeController.java
+++ src/main/java/com/lazyyy/blog/controller/HomeController.java
@@ -13,8 +13,6 @@ public class HomeController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h3>Hello World!</h3>");
+        request.getRequestDispatcher("/WEB-INF/views/HomeScreen.jsp").forward(request,response);
     }
 }
--- target/Blog/WEB-INF/views/HomeScreen.jsp
+++ target/Blog/WEB-INF/views/HomeScreen.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-
+    <h1>ABCDE</h1>
 </body>
 </html>
